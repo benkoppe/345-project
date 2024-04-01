21be5192571499d14d0e221b57629fa5a5ee9206
Ben <Koppe.Development@gmail.com>
2024-04-01 05:34:44 -0700

    added Main for testing

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import core.Maze;
+
+public class Main {
+    public static void main(String[] args) {
+        Maze maze = new Maze(10, 10);
+
+        System.out.println(maze.nodes.size());
+    }
+}

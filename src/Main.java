677c9b2501c0dabd1340866af0a72deae60a6e5f
Ben <Koppe.Development@gmail.com>
2024-04-01 05:35:04 -0700

    testing comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import core.Maze;
 
 public class Main {
     public static void main(String[] args) {
+        // later, this will be the main entry point, but for now, we're just testing
+        
         Maze maze = new Maze(10, 10);
 
         System.out.println(maze.nodes.size());

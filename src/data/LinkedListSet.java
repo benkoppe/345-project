33beb663278645b160f4e5fd56f11281045d6b3d
Ben <Koppe.Development@gmail.com>
2024-04-04 03:37:45 -0700

    added getting random values, iterating, and filtering

--- src/data/LinkedListSet.java
+++ src/data/LinkedListSet.java
@@ -2,6 +2,7 @@ package data;
 
 public class LinkedListSet<T> {
     private Node head;
+    private int size;
 
     private class Node {
         T data;
@@ -15,6 +16,7 @@ public class LinkedListSet<T> {
 
     public LinkedListSet() {
         this.head = null;
+        this.size = 0;
     }
 
     // Add a new element to the set if it's not already present
@@ -23,6 +25,7 @@ public class LinkedListSet<T> {
             Node newNode = new Node(data);
             newNode.next = head;
             head = newNode;
+            size++;
         }
     }
 
@@ -46,6 +49,7 @@ public class LinkedListSet<T> {
         // If the node is to be removed from the head
         if (head.data.equals(data)) {
             head = head.next;
+            size--;
             return;
         }
 
@@ -53,6 +57,7 @@ public class LinkedListSet<T> {
         while (current.next != null) {
             if (current.next.data.equals(data)) {
                 current.next = current.next.next;
+                size--;
                 return; // Data found and removed
             }
             current = current.next;
@@ -66,5 +71,37 @@ public class LinkedListSet<T> {
             System.out.println(current.data);
             current = current.next;
         }
+    }
+
+    public void forEach(Runnable<T> action) {
+        Node current = head;
+        while (current != null) {
+            action.run(current.data);
+            current = current.next;
+        }
+    }
+
+    public LinkedListSet<T> filter(Condition<T> condition) {
+        LinkedListSet<T> filteredSet = new LinkedListSet<>();
+        Node current = head;
+        while (current != null) {
+            if (condition.test(current.data)) {
+                filteredSet.add(current.data);
+            }
+            current = current.next;
+        }
+        return filteredSet;
+    }
+
+    public T randomElement() {
+        if (size == 0) {
+            return null;
+        }
+        int randomIndex = (int) (Math.random() * size);
+        Node current = head;
+        for (int i = 0; i < randomIndex; i++) {
+            current = current.next;
+        }
+        return current.data;
     }
 }

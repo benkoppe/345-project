0c60eb15c8c396dc7f373b5f74ffc962ba3e3d2b
Ben <Koppe.Development@gmail.com>
2024-04-04 06:52:57 -0700

    moved functionals to their own folder

--- src/data/functionals/Condition.java
+++ src/data/functionals/Condition.java
@@ -1,4 +1,4 @@
-package data;
+package data.functionals;
 
 @FunctionalInterface
 public interface Condition<T> {
--- src/data/functionals/Runnable.java
+++ src/data/functionals/Runnable.java
@@ -1,4 +1,4 @@
-package data;
+package data.functionals;
 
 @FunctionalInterface
 public interface Runnable<T> {

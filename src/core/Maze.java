90dca0a819c0fccc1277a95af4f9045e730f0c49
Ben <Koppe.Development@gmail.com>
2024-04-04 03:38:11 -0700

    added printing maze

--- src/core/Maze.java
+++ src/core/Maze.java
@@ -5,12 +5,16 @@ import data.LinkedListSet;
 
 public class Maze {
     public Node[] nodes;
+    private int rows;
+    private int cols;
 
     public Maze(int rows, int cols) {
         this.nodes = createNodes(rows, cols);
     }
     
     private Node[] createNodes(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
         Node[] nodes = new Node[rows * cols];
 
         for (int i = 0; i < rows; i++) {
@@ -49,5 +53,34 @@ public class Maze {
         }
 
         return new Node(id, neighbors);
+    }
+
+    public void printMaze() {
+        System.out.println();
+        // run through the first row, printing the top walls
+        for (int i = 0; i < cols; i++) {
+            System.out.print("__");
+        }
+        // run through the other rows, printing the side and bottom walls
+        for (int i = 0; i < rows; i++) {
+            System.out.println();
+            System.out.print("|");
+            for (int j = 0; j < cols; j++) {
+                int id = i * cols + j;
+                Node current = nodes[id];
+
+                if (current.connections.contains(id + cols)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("_");
+                }
+
+                if (current.connections.contains(id + 1)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("|");
+                }
+            }
+        }
     }
 }

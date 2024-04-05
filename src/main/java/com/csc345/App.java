adb946f793b51453dd10660092c8d30ae8fae8ce
Ben <Koppe.Development@gmail.com>
2024-04-05 00:52:43 -0700

    removed fxml dependency

--- src/main/java/com/csc345/App.java
+++ src/main/java/com/csc345/App.java
@@ -1,12 +1,11 @@
 package com.csc345;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import com.csc345.gui.MazeImage;
 
 /**
  * JavaFX App
@@ -16,23 +15,17 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) {
+        MazeImage root = new MazeImage(10, 10);
+        Group group = new Group(root);
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        scene = new Scene(group, 640, 480);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }

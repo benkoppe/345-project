934b3718b9ea6ee03e65efcd637e326244351d36
Ben <Koppe.Development@gmail.com>
2024-04-09 00:30:38 -0700

    updated main to new framework

--- src/main/java/com/csc345/Main.java
+++ src/main/java/com/csc345/Main.java
@@ -2,38 +2,35 @@ package com.csc345;
 
 import com.csc345.core.Maze;
 import com.csc345.core.Node;
-import com.csc345.core.maze_algorithms.algorithms.Backtracking;
-import com.csc345.core.solve_algorithms.algorithms.AStar;
+import com.csc345.core.maze_algorithms.MazeAlgorithmType;
+import com.csc345.core.maze_algorithms.MazeAlgorithm;
+import com.csc345.core.solve_algorithms.SolveAlgorithmType;
+import com.csc345.core.solve_algorithms.SolveAlgorithm;
 
 import com.csc345.data.RowCol;
 import com.csc345.data.List;
 
 public class Main {
     public static void main(String[] args) {
-        // later, this will be the main entry point, but for now, we're just testing
+        // example programmatic maze creation and solving
         
         RowCol mazeSize = new RowCol(10, 10);
-        Node[] maze = Maze.createNodes(mazeSize.getRow(), mazeSize.getCol());
+        Node[] nodes = Maze.createNodes(mazeSize.getRow(), mazeSize.getCol());
         
-        Maze.printMaze(maze, mazeSize.getCol());
+        Maze.printMaze(nodes, mazeSize.getCol());
 
-        Backtracking backtracking = new Backtracking(maze);
+        MazeAlgorithm backtracking = MazeAlgorithmType.BACKTRACKING.getAlgorithm(nodes);
         backtracking.finishImmediately();
 
-        Maze.printMaze(maze, mazeSize.getCol());
+        Maze.printMaze(nodes, mazeSize.getCol());
 
-        AStar aStar = new AStar(maze, 0, maze.length - 1, (startNodeId, endNodeId) -> {
-            RowCol startRowCol = RowCol.idToRowCol(startNodeId, mazeSize.getCol());
-            RowCol endRowCol = RowCol.idToRowCol(endNodeId, mazeSize.getCol());
-
-            return (double) (Math.abs(startRowCol.getRow() - endRowCol.getRow()) + Math.abs(startRowCol.getCol() - endRowCol.getCol()));
-        });
+        SolveAlgorithm aStar = SolveAlgorithmType.ASTAR.getAlgorithm(nodes, 0, nodes.length - 1, Maze.createHeuristic(mazeSize.getCol()));
 
         aStar.finishImmediately();
 
         List<Integer> path = aStar.getPath();
         
-        Maze.printMazeSolution(path, maze, mazeSize.getCol());
+        Maze.printMazeSolution(path, nodes, mazeSize.getCol());
 
     }
 }

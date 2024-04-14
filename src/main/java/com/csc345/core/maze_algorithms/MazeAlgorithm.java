80edf07c19987f6b4f24aae2271d4903d30e868a
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-14 16:48:05 -0700

    Update MazeAlgorithm.java

--- src/main/java/com/csc345/core/maze_algorithms/MazeAlgorithm.java
+++ src/main/java/com/csc345/core/maze_algorithms/MazeAlgorithm.java
@@ -4,6 +4,21 @@ import com.csc345.core.Algorithm;
 import com.csc345.core.State;
 import com.csc345.core.Node;
 
+/**
+ * This abstract class serves as a starting point for all maze algorithms. It extends the Algorithm class,
+ * which means it inherits all its properties and methods, but here we're specifically focusing on mazes.
+ * When we create a new maze algorithm, we start by setting all nodes to UNVISITED, so we know we haven't 
+ * been there yet.
+ */
+
+/**
+ * It initializes a new MazeAlgorithm with a given array of nodes.
+ * Sets each node's state to UNVISITED to indicate that none of the nodes have been explored yet.
+ * This setup is crucial for starting the maze generation process properly.
+ *
+ * @param nodes An array of nodes to be used in the maze. Each node should already be instantiated before it is passed here.
+ *              The constructor marks all these nodes as UNVISITED.
+ */
 public abstract class MazeAlgorithm extends Algorithm {
 
     public MazeAlgorithm(Node[] nodes) {

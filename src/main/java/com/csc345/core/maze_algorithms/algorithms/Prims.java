2fb0ac00b47e0e2680f8915c4563ccffd00f5eec
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-15 02:04:11 -0700

    Update Prims.java

--- src/main/java/com/csc345/core/maze_algorithms/algorithms/Prims.java
+++ src/main/java/com/csc345/core/maze_algorithms/algorithms/Prims.java
@@ -5,9 +5,19 @@ import com.csc345.core.State;
 import com.csc345.core.maze_algorithms.MazeAlgorithm;
 import com.csc345.data.LinkedListSet;
 
+/**
+ * This class uses Prim's algorithm to generate a maze. Starting from a random node,
+ * it expands the maze by adding one frontier node at a time. It's a bit like growing a tree
+ * where each new branch is chosen randomly from the edges that reach out into unexplored areas.
+ */
 public class Prims extends MazeAlgorithm {
 
     private LinkedListSet<Integer> fronteirs;
+    /**
+     * Constructor that starts the maze generation from a random node and prepares the frontier set.
+     *
+     * @param nodes Array of nodes in the maze. These are the "rooms" or "cells" of our maze.
+     */
 
     public Prims(Node[] nodes) {
         super(nodes);
@@ -21,7 +31,12 @@ public class Prims extends MazeAlgorithm {
             changeState(neighborId, State.VISITING);
         });
     }
-
+    /**
+     * Expands the maze by one node each time it is called. It randomly picks a frontier node,
+     * connects it to the maze, and adds its unvisited neighbors to the frontiers.
+     *
+     * @return true if there are no more frontier nodes to process, false otherwise.
+     */
     @Override
     protected boolean loopOnceInternal() {
         Node randomFronteir = nodes[fronteirs.randomElement()];

9616902e3942cf6145aec366105c9615468b49bb
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-15 02:56:48 -0700

    Update AStar.java

--- src/main/java/com/csc345/core/solve_algorithms/algorithms/AStar.java
+++ src/main/java/com/csc345/core/solve_algorithms/algorithms/AStar.java
@@ -7,6 +7,11 @@ import com.csc345.data.functionals.BiFunction;
 import com.csc345.core.Node;
 import com.csc345.core.State;
 
+/**
+ * This class uses the A* algorithm to find the shortest path from a start node to an end node.
+ * It’s pretty cool because it uses a heuristic to guess how close we are to the goal, making it
+ * smarter and faster than some other pathfinding algorithms.
+ */
 public class AStar extends SolveAlgorithm {
     
     private HashMap<Integer, Integer> cameFrom = new HashMap<>();
@@ -15,7 +20,15 @@ public class AStar extends SolveAlgorithm {
 
     private PriorityQueue<Integer, Double> openQueue;
     private BiFunction<Integer, Integer, Double> heuristic;
-
+    
+    /**
+     * Sets up the A* algorithm with a specific start node, end node, and heuristic.
+     *
+     * @param nodes An array of all nodes in the graph.
+     * @param startId The ID of the start node.
+     * @param endId The ID of the end node.
+     * @param heuristic A function that estimates the cost from any node to the end node.
+     */
     public AStar(Node[] nodes, int startId, int endId, BiFunction<Integer, Integer, Double> heuristic) {
         super(nodes, startId, endId);
         this.heuristic = heuristic;
@@ -27,7 +40,12 @@ public class AStar extends SolveAlgorithm {
         fScores.put(startId, heuristic.apply(startId, endId));
         changeState(startId, State.VISITING);
     }
-
+    /**
+     * Runs one iteration of the A* algorithm. It picks the node with the lowest estimated cost,
+     * checks if it’s the end node, and if not, it processes its neighbors and updates their scores.
+     *
+     * @return true if the end node is reached or the open queue is empty (no path found), false otherwise.
+     */
     @Override
     protected boolean loopOnceInternal() {
         if (openQueue.isEmpty()) {

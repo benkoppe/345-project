6491a4be781148dab89d0e26db38c020a97f922a
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-15 03:04:20 -0700

    Update Breadth.java

--- src/main/java/com/csc345/core/solve_algorithms/algorithms/Breadth.java
+++ src/main/java/com/csc345/core/solve_algorithms/algorithms/Breadth.java
@@ -7,12 +7,24 @@ import com.csc345.core.solve_algorithms.SolveAlgorithm;
 import com.csc345.data.ArrayDeque;
 import com.csc345.data.HashMap;
 
+/**
+ * This class implements the Breadth-First Search (BFS) algorithm to find the shortest path in a graph.
+ * It's like exploring a maze by checking all the rooms (nodes) level by level, starting from the entrance
+ * (start node) until we find the exit (end node).
+ */
 public class Breadth extends SolveAlgorithm {
 
     private ArrayDeque queue;
     private HashMap<Integer, Integer> cameFrom;
     
-
+    /**
+     * Sets up the BFS with a start and an end node. It initializes the queue and marks the start node
+     * as visiting to kick off the search.
+     *
+     * @param nodes An array of all nodes in the graph.
+     * @param startId The ID of the start node.
+     * @param endId The ID of the end node.
+     */
     public Breadth(Node[] nodes, int startId, int endId) {
         super(nodes, startId, endId);
 
@@ -22,7 +34,12 @@ public class Breadth extends SolveAlgorithm {
         changeState(startId, State.VISITING);
         queue.addLast(startId);
     }
-
+    /**
+     * Runs one cycle of the BFS algorithm. It processes each node in the queue by checking its neighbors,
+     * and if they haven't been visited yet, adds them to the queue for the next cycle.
+     *
+     * @return true if the end node is reached (path found), false if more nodes need to be processed.
+     */
     @Override
     protected boolean loopOnceInternal() {
         ArrayDeque nextQueue = new ArrayDeque(queue.getSize());

ad597fa0ca09926e4a664d369dc8e9f4ce80a205
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-15 03:11:52 -0700

    Update Tremaux.java

--- src/main/java/com/csc345/core/solve_algorithms/algorithms/Tremaux.java
+++ src/main/java/com/csc345/core/solve_algorithms/algorithms/Tremaux.java
@@ -5,8 +5,21 @@ import com.csc345.core.State;
 import com.csc345.core.solve_algorithms.SolveAlgorithm;
 import com.csc345.data.LinkedListSet;
 
+/**
+ * This class implements Trémaux's algorithm, a cool old-school maze-solving method.
+ * It works kind of like leaving breadcrumbs: you mark where you've been so you don't
+ * go down the same path twice unless you need to backtrack.
+ */
 public class Tremaux extends SolveAlgorithm {
     
+    /**
+     * Sets up the maze with the starting point marked as visiting. This is where
+     * the journey through the maze begins.
+     *
+     * @param nodes An array of all nodes in the maze.
+     * @param startId The ID of the node where we start.
+     * @param endId The ID of the node where we hope to end up.
+     */
 
     public Tremaux(Node[] nodes, int startId, int endId) {
         super(nodes, startId, endId);
@@ -14,7 +27,13 @@ public class Tremaux extends SolveAlgorithm {
         path.add(startId);
         changeState(startId, State.VISITING);
     }
-
+    /**
+     * Does one step of Trémaux's algorithm. It checks for unvisited paths from the
+     * current position and chooses one to move forward. If no unvisited paths are left,
+     * it backtracks to a previous node.
+     *
+     * @return true if we reach the end node, false if the journey must continue.
+     */
     @Override
     protected boolean loopOnceInternal() {
         int currentId = path.getLast();

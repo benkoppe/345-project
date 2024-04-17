baaa21a1077876b3a6a35c5a839b2d8530d432df
yashiguptaa <148113241+yashiguptaa@users.noreply.github.com>
2024-04-16 22:20:49 -0700

    Update AlgorithmType.java

--- src/main/java/com/csc345/core/AlgorithmType.java
+++ src/main/java/com/csc345/core/AlgorithmType.java
@@ -1,8 +1,19 @@
 package com.csc345.core;
 
 public interface AlgorithmType {
+    /**
+     * retrieves the name of the algorithm type.
+     * 
+     * @return A {@code String} representing the name of the algorithm type.
+     */
     public String getName();
-
+    /**
+     * it returns a default value for the AlgorithmType. This method can be used to provide
+     * a default algorithm type when no specific implementation is required.
+     * 
+     * @return An {@code AlgorithmType} representing the default algorithm type. Can be
+     *         {@code null} if no default type is specified.
+     */
     public static AlgorithmType defaultValue() {
         return null;
     };

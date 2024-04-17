2b1a8e303b09a961bc21170b08f3e122b752b892
Ben <Koppe.Development@gmail.com>
2024-04-16 22:59:50 -0700

    detailed documentation for SolveAlgorithmType

--- src/main/java/com/csc345/core/solve_algorithms/SolveAlgorithmType.java
+++ src/main/java/com/csc345/core/solve_algorithms/SolveAlgorithmType.java
@@ -6,6 +6,9 @@ import com.csc345.core.solve_algorithms.algorithms.*;
 
 import com.csc345.data.functionals.BiFunction;
 
+/**
+ * Enum for all SolveAlgorithm subclasses, called Types.
+ */
 public enum SolveAlgorithmType implements AlgorithmType {
     ASTAR("A-Star"),
     DIJKSTRA("Dijkstra"),
@@ -14,19 +17,43 @@ public enum SolveAlgorithmType implements AlgorithmType {
 
     private final String name;
 
+    /**
+     * Constructor for SolveAlgorithmType. Requires a name.
+     * 
+     * @param name name of the algorithm
+     */
     SolveAlgorithmType(String name) {
         this.name = name;
     }
 
+    /**
+     * Returns the name of the algorithm.
+     * 
+     * @return the name of the algorithm
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Converts the enum to a string.
+     * 
+     * @return the name of the algorithm
+     */
     @Override
     public String toString() {
         return name;
     }
 
+    /**
+     * Converts the enum to an initialized SolveAlgorithm
+     * 
+     * @param nodes the maze nodes to solve
+     * @param startId the start node id
+     * @param endId the end node id
+     * @param heuristicFunction the heuristic function (for A-Star)
+     * @return the initialized SolveAlgorithm
+     */
     public SolveAlgorithm getAlgorithm(Node[] nodes, int startId, int endId, BiFunction<Integer, Integer, Double> heuristicFunction) {
         switch (this) {
             case ASTAR:
@@ -42,6 +69,11 @@ public enum SolveAlgorithmType implements AlgorithmType {
         }
     }
 
+    /**
+     * Returns the default SolveAlgorithmType.
+     * 
+     * @return the default SolveAlgorithmType
+     */
     public static SolveAlgorithmType defaultValue() {
         return ASTAR;
     }

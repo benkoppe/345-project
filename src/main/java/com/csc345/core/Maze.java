d6cfb83f111dd4faeae695ed8486a3e06cf834fb
Ben <Koppe.Development@gmail.com>
2024-04-16 22:21:14 -0700

    detailed documentation for Maze

--- src/main/java/com/csc345/core/Maze.java
+++ src/main/java/com/csc345/core/Maze.java
@@ -1,11 +1,23 @@
 package com.csc345.core;
 
-import com.csc345.data.RowCol;
 import com.csc345.data.LinkedListSet;
 import com.csc345.data.List;
 import com.csc345.data.functionals.BiFunction;
+import com.csc345.data.types.RowCol;
 
+/**
+ * Class of static maze-related methods.
+ */
 public class Maze {
+
+    /**
+     * Creates a maze, represented as an array of nodes.
+     * Each node stores its neighbors, and no connections are made.
+     * 
+     * @param rows number of rows in the maze
+     * @param cols number of columns in the maze
+     * @return array of nodes representing the maze
+     */
     public static Node[] createNodes(int rows, int cols) {
         Node[] nodes = new Node[rows * cols];
 
@@ -19,6 +31,14 @@ public class Maze {
         return nodes;
     }
 
+    /**
+     * Create a new Node instance at a given id in a maze
+     * 
+     * @param id id of the node
+     * @param rows number of rows in the maze
+     * @param cols number of columns in the maze
+     * @return new Node instance
+     */
     private static Node createNode(int id, int rows, int cols) {
         LinkedListSet<Integer> neighbors = new LinkedListSet<>();
 
@@ -47,6 +67,13 @@ public class Maze {
         return new Node(id, neighbors);
     }
 
+    /**
+     * Creates a heuristic function for a maze of given size, based on the Manhattan distance.
+     * Used in solving (A-Star) methods
+     * 
+     * @param cols number of columns in the maze
+     * @return heuristic bifunction, which takes in the start and end node ids and returns the heuristic value
+     */
     public static BiFunction<Integer, Integer, Double> createHeuristic(int cols) {
         return (startNodeId, endNodeId) -> {
             RowCol startRowCol = RowCol.idToRowCol(startNodeId, cols);
@@ -56,6 +83,12 @@ public class Maze {
         };
     }
 
+    /**
+     * Prints a maze that's represented as an array of nodes to the console.
+     * 
+     * @param nodes array of nodes representing the maze
+     * @param cols number of columns in the maze
+     */
     public static void printMaze(Node[] nodes, int cols) {
         int rows = nodes.length / cols;
 
@@ -87,6 +120,13 @@ public class Maze {
         }
     }
 
+    /**
+     * Prints a maze with its solution to the console.
+     * 
+     * @param path list of node ids in the solution path
+     * @param nodes array of nodes representing the maze
+     * @param cols number of columns in the maze
+     */
     public static void printMazeSolution(List<Integer> path, Node[] nodes, int cols) {
         int rows = nodes.length / cols;
 
